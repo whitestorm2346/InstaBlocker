dc7932b96c01e664c92ae4bf78f2fca1c1b3ddd1
whitestorm2346 <andrew770426@gmail.com>
2024-06-02 03:19:55 +0800

    Finished the project

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as chromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException, NoSuchElementException
 import chromedriver_autoinstaller
 import pandas as pd
 
-from colorama import init, Fore, Back, Style
+from colorama import init, Fore, Style
 
-DATA_FILE = 'test.xlsx'
+DATA_FILE = 'qusun.ny-劣質粉絲名單.xlsx'
 
 INSTA_LOGIN_URL = 'https://www.instagram.com/?hl=zh' 
 AUTHENTICATE_URL = 'https://www.instagram.com/accounts/login/two_factor?hl=zh&next=%2F'
 
 ENGLISH = 'Times New Roman'
 CHINESE = '微軟正黑體'
+
+is_aborted = False
 
 class CheckLogin:
     def __init__(self) -> None:
@@ -26,24 +29,28 @@ class CheckLogin:
         self.__init_buttons__()
  
     def __start_button_onclick__(self):
-        print('start button onclick')
         self.root.destroy()
 
     def __init_root__(self):
         self.root = Tk()
         self.root.resizable(False, False)
-        self.root.geometry("300x300")
+        self.root.geometry("300x180")
         self.root.title('InstaBlocker Check Login')
 
     def __init_label__(self):
-        self.label = Label(self.root)
-        self.label.config(
-            text='Please finish the login process, then click the button below.', font=(ENGLISH, 14, 'bold'))
-        self.label.pack(side=TOP, pady=5)
+        self.label1 = Label(self.root)
+        self.label1.config(
+            text='Please finish the login process,', font=(ENGLISH, 14, 'bold'))
+        self.label1.pack(side=TOP, pady=5)
+        
+        self.label2 = Label(self.root)
+        self.label2.config(
+            text='then click the button below.', font=(ENGLISH, 14, 'bold'))
+        self.label2.pack(side=TOP, pady=5)
 
     def __init_buttons__(self):
         self.control_frame = Frame(self.root)
-        self.control_frame.pack(side=TOP, fill='x', padx=15, pady=0)
+        self.control_frame.pack(side=BOTTOM, fill='x', padx=15, pady=10)
 
         self.start_btn = Button(self.control_frame)
         self.start_btn.config(text='start', font=(ENGLISH, 14, 'bold'),
@@ -54,12 +61,10 @@ class CheckLogin:
         self.root.mainloop()
 
 class BlockadeLogView:
-    def __init__(self, is_abort) -> None:
+    def __init__(self) -> None:
         self.__init_root__()
         self.__init_inner_frame__()
         self.__init_buttons__()
-
-        self.is_aborted = is_abort
 
     def __init_root__(self):
         self.root = Tk()
@@ -76,7 +81,12 @@ class BlockadeLogView:
             self.inner_canvas.config(width=self.window_frame.winfo_reqwidth())
 
     def __abort_blockade__(self):
-        self.is_aborted = True
+        global is_aborted
+
+        print(Fore.YELLOW + 'abort button on click')
+        is_aborted = True
+
+        self.root.destroy()
 
     def __init_inner_frame__(self):
         self.inner_frame = Frame(self.root)
@@ -123,14 +133,7 @@ class InstaBlocker:
         
     def __init_path__(self):
         self.path = {
-            # BY XPATH
-            'username_input': '//*[@id="loginForm"]/div/div[1]/div/label/input',
-            'password_input': '//*[@id="loginForm"]/div/div[2]/div/label/input',
-            'login_button': '//button[normalize-space()="登入"]',
-            'verify_code_input': '/html/body/div[2]/div/div/div[2]/div/div/div[1]/section/main/div/div/div[1]/div[2]/form/div[1]/div/label/input',
-            'confirm_button': '//button[normalize-space()="確認"]',
-            'save_data_later_button': '//button[normalize-space()="稍後再說"]',
-            'turn_on_notifications_button': '//button[normalize-space()="開啟通知"]',
+            # BY XPATH        
             'blockade_button': '//button[normalize-space()="封鎖"]',
             'remove_blockade_button': '//button[normalize-space()="解除封鎖"]',
             'cancel_blockade_button': '//button[normalize-space()="取消"]',
@@ -143,15 +146,17 @@ class InstaBlocker:
 
     def __init_data__(self):
         try:
-            self.data = pd.read_excel('qusun.ny-劣質粉絲名單.xlsx', header=7)
+            self.data = pd.read_excel(DATA_FILE, header=7)
         except Exception as e:
-            print('qusun.ny-劣質粉絲名單.xlsx not found')
+            print(f'{DATA_FILE} not found')
             exit(1)
 
-        self.namelist = self.data[self.data['評分'] <= -55][self.data.columns[1]].tolist()
+        self.data = self.data[self.data['評分'] <= -55]
+        self.namelist = self.data[self.data.columns[1]].tolist()
 
     def __update_data__(self):
         header = pd.read_excel(DATA_FILE, nrows=7)
+        self.data = self.data[self.data[self.data.columns[1]].isin(self.namelist)]
 
         with pd.ExcelWriter(DATA_FILE, engine='openpyxl') as writer:
             header.to_excel(writer, index=False)
@@ -164,50 +169,70 @@ class InstaBlocker:
         chromedriver_autoinstaller.install()
         self.driver = webdriver.Chrome(options=chrome_option)
         
-    def blockade(self, namelist) -> int:
-        for username in namelist[:]:
-            print(Fore.GREEN + f"{self.is_aborted}\n")
+    def blockade(self) -> int:
+        global is_aborted
 
-            if self.is_aborted:
+        blockced = []
+
+        for username in self.namelist:
+            url_split = username.split('/')
+
+            if is_aborted:
                 break
 
-            self.driver.get(username)
+            print('\n' + Style.BRIGHT + '[account] ' + Fore.CYAN + f'{url_split[-1]}')
 
-            url_split = username.split('/')
+            self.driver.get(username)
 
             try:
                 options_btn = WebDriverWait(self.driver, 5).until(
                     EC.presence_of_element_located((By.CSS_SELECTOR, 
                     self.path['options_button'])))
                 options_btn.click()
-            except Exception as e:
-                print('options_btn not found')
-                print(format(e))
-                self.blockade_log_view.new_log(f'{url_split[-1]} error: options_btn not found')
+            except TimeoutException as e:
+                try:
+                    print(Style.BRIGHT + '[status] ' + Fore.LIGHTRED_EX + 'account not found')
+                    self.blockade_log_view.new_log(f'{url_split[-1]} error: account not found')
+                except Exception as e:
+                    pass
+
+                blockced.append(username)
+
                 continue
 
             try:
                 remove_blockade_btn = WebDriverWait(self.driver, 1).until(
                     EC.presence_of_element_located((By.XPATH, 
                     self.path['remove_blockade_button'])))
-                self.blockade_log_view.new_log(f'{url_split[-1]} blocked already')
+                
+                try:
+                    print(Style.BRIGHT + '[status] ' + Fore.YELLOW + 'blocked already')
+                    self.blockade_log_view.new_log(f'{url_split[-1]} blocked already')
+                except Exception as e:
+                    pass
 
                 try:
                     cancel_blockade_btn = self.driver.find_element(By.XPATH, self.path['cancel_blockade_button'])
                     cancel_blockade_btn.click()
-                except Exception as e:
-                    print(format(e))
+                except NoSuchElementException as e:
+                    print(Fore.RED + 'cancel_blockade_btn not found')
+
+                blockced.append(username)
 
                 continue
-            except Exception as e:
+            except TimeoutException as e:
                 try:
                     blockade_btn = WebDriverWait(self.driver, 1).until(
                         EC.presence_of_element_located((By.XPATH, 
                         self.path['blockade_button'])))
-                except Exception as e:
-                    print('blockade_btn not found')
-                    print(format(e))
-                    self.blockade_log_view.new_log(f'{url_split[-1]} error: blockade_btn not found')
+                except TimeoutException as e:
+                    print(Fore.RED + 'blockade_btn not found')
+
+                    try:
+                        self.blockade_log_view.new_log(f'{url_split[-1]} error: blockade_btn not found')
+                    except Exception as e:
+                        pass
+
                     continue
             
             blockade_btn.click()
@@ -219,12 +244,22 @@ class InstaBlocker:
                 btns = div.find_elements(By.TAG_NAME, 'button')
                 btns[0].click()
 
-                self.blockade_log_view.new_log(f'{url_split[-1]} are blocked')
-                namelist.remove(username)
-            except Exception as e:
-                print('check_blockade_btn not found')
-                print(format(e))
-                self.blockade_log_view.new_log(f'{url_split[-1]} error: check_blockade_btn not found')
+                print(Style.BRIGHT + '[status] ' + Fore.LIGHTGREEN_EX + 'blockade success')
+
+                try:
+                    self.blockade_log_view.new_log(f'{url_split[-1]} are blocked')
+                except Exception as e:
+                    pass
+
+                blockced.append(username)
+            except TimeoutException as e:
+                print(Fore.RED + 'check_blockade_btn not found')
+
+                try:
+                    self.blockade_log_view.new_log(f'{url_split[-1]} error: check_blockade_btn not found')
+                except Exception as e:
+                    pass
+
                 continue
 
             try:
@@ -232,41 +267,52 @@ class InstaBlocker:
                     EC.presence_of_element_located((By.XPATH, 
                     self.path['close_blockade_button'])))
                 close_blockade_btn.click()
-            except Exception as e:
-                print('close_blockade_btn not found')
-                print(format(e))
-                self.blockade_log_view.new_log(f'{url_split[-1]} error: close_btn not found')
+            except TimeoutException as e:
+                print(Fore.RED + 'close_blockade_btn not found')
+
+                try:
+                    self.blockade_log_view.new_log(f'{url_split[-1]} error: close_btn not found')
+                except Exception as e:
+                    pass
+
                 continue
 
-        self.namelist = namelist
+        self.namelist = list(set(self.namelist) - set(blockced))
             
         return 0
 
     def __check_login__(self):
         self.check_login = CheckLogin()
         self.check_login.run()
-    
-    def __run_blockade__(self):
-        self.blockade(self.namelist)
 
     def run(self) -> None:
+        global is_aborted
+
         self.start_driver()
 
-        check_login_threading = threading.Thread(target=self.__run_blockade__)
+        self.driver.get(INSTA_LOGIN_URL)
+
+        check_login_threading = threading.Thread(target=self.__check_login__)
         check_login_threading.start()
         check_login_threading.join()
 
-        self.is_aborted = False
+        is_aborted = False
 
-        blockade_thread = threading.Thread(target=self.__run_blockade__)
+        blockade_thread = threading.Thread(target=self.blockade)
         blockade_thread.start()
 
-        self.blockade_log_view = BlockadeLogView(self.is_aborted)
+        self.blockade_log_view = BlockadeLogView()
         self.blockade_log_view.run()
 
         blockade_thread.join()
-        self.blockade_log_view.root.destroy()
+
+        self.driver.close()
+
+        print(Fore.YELLOW + 'Updating Data...')
+
         self.__update_data__()
+
+        print(Fore.YELLOW + 'Finished Updating...')
 
 
 if __name__ == "__main__":
